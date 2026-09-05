6acaf47c1b5a2c643d51868e15859ae4aa32d80c
L1ghtzin <lightzin021@gmail.com>
2026-09-05 16:23:22 -0300

    Sanders: Restore vibrator support

--- device.mk
+++ device.mk
@@ -12,3 +12,7 @@ PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
+
+# Vibrator AIDL V1 compatibility for the prebuilt Qualcomm service
+TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.vibrator-V1-ndk_platform.so
